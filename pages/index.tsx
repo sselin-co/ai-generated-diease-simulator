e917b303749f8d39251036f4b6995e1f48c60daa
Shaya Selincourt <sselincourt@gmail.com>
2023-04-08 23:35:50 -0700

    Working simulation with basic controls and configuration history

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,93 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React, { useState } from 'react';
+import Layout from '../components/Layout';
+import Simulation from '../components/simulation/Simulation';
+import Controls from '../components/controls/Controls';
+import SimulationHistory from '@/components/history/SimulationHistory';
+import { SimulationConfiguration } from '@/components/simulation/SimulationConfiguration';
 
-const inter = Inter({ subsets: ['latin'] })
+type SimulationConfig = {
+  population: number;
+  infectivityRadius: number;
+  probabilityOfInfection: number;
+  agentSpeed: number;
+};
 
-export default function Home() {
+const DEFAULT_CONFIG: SimulationConfig = {
+  population: 50,
+  infectivityRadius: 1,
+  probabilityOfInfection: 0.8,
+  agentSpeed: 0.1,
+};
+
+const Home: React.FC = () => {
+  // State for agentSpeed control.
+  const [agentSpeed, setAgentSpeed] = useState(DEFAULT_CONFIG.agentSpeed);
+  const [population, setPopulation] = useState(DEFAULT_CONFIG.population);
+  const [infectivityRadius, setInfectivityRadius] = useState(DEFAULT_CONFIG.infectivityRadius);
+  const [probabilityOfInfection, setProbabilityOfInfection] = useState(DEFAULT_CONFIG.probabilityOfInfection);
+  // Add a state to store the simulation history.
+  const [simulationHistory, setSimulationHistory] = useState<SimulationConfiguration[]>([]);
+
+  const handleSaveConfiguration = () => {
+    const configuration: SimulationConfiguration = {
+      id: Date.now(),
+      timestamp: new Date().toLocaleString(),
+      agentSpeed,
+      population,
+      infectivityRadius,
+      probabilityOfInfection,
+      history: simulationHistory,
+    };
+
+    setSimulationHistory([...simulationHistory, configuration]);
+  };
+
+
+  const handleLoadConfiguration = (configuration: SimulationConfiguration) => {
+    setAgentSpeed(configuration.agentSpeed);
+    setPopulation(configuration.population)
+    setInfectivityRadius(configuration.infectivityRadius)
+    setProbabilityOfInfection(configuration.probabilityOfInfection)
+    // Load other configuration parameters here
+
+    setSimulationHistory([...simulationHistory])
+  };
+
+  const handleClearHistory = () => {
+    setSimulationHistory([]);
+  };
+
+  // Add additional states for other controls as needed.
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <Layout>
+      <Simulation
+        agentSpeed={agentSpeed}
+        population={population}
+        infectivityRadius={infectivityRadius}
+        probabilityOfInfection={probabilityOfInfection}
+      />
+      <Controls
+        agentSpeed={agentSpeed}
+        setAgentSpeed={setAgentSpeed}
+        population={population}
+        setPopulation={setPopulation}
+        infectivityRadius={infectivityRadius}
+        setInfectivityRadius={setInfectivityRadius}
+        probabilityOfInfection={probabilityOfInfection}
+        setProbabilityOfInfection={setProbabilityOfInfection}
+        onSave={handleSaveConfiguration}
+      />
+      <div className="col-span-1" style={{ minHeight: '10rem' }}>
+        <SimulationHistory
+          history={simulationHistory}
+          onLoadConfiguration={handleLoadConfiguration}
+          onClearHistory={handleClearHistory}
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    </Layout>
+  );
+};
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default Home;
